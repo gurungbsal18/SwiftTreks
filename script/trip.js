7925467ca6e292a6028ccc911b1404d20d245104
gurungbsal18 <gurungbsal18@gmail.com>
2023-08-03 22:34:33 +0545

    sidebar price box added

--- script/trip.js
+++ script/trip.js
@@ -22,3 +22,11 @@ costExcludeBtn.addEventListener("click", function () {
   costIncludeBtn.classList.add("btn-outline-success");
   costIncludeBtn.classList.remove("btn-success");
 });
+
+// date picker
+document.addEventListener("DOMContentLoaded", function () {
+  flatpickr("#dateInput", {
+    dateFormat: "Y-m-d", // Format of the selected date (change as needed)
+    enableTime: false, // Set to true if you want to enable time selection as well
+  });
+});
