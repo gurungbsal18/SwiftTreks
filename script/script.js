dafcc9c60d694392fda3724f4c06c2df3eba9946
gurungbsal18 <gurungbsal18@gmail.com>
2023-08-11 01:13:48 +0545

    read more or less of explore region btn fixed

--- script/script.js
+++ script/script.js
@@ -1,17 +1,13 @@
-// explore region page read more and less button
-let exploreRegionContent = document.querySelector(".region-content");
-let exploreRegionContentBtn = document.querySelector(".region-btn button"); // Select the button element
-let exploreRegionContentButton = document.querySelector(".region-btn"); // Select the button element
+// /read more and less of explore region 
+let readMoreBtn = document.getElementById('readMoreBtn');
+let regionContent = document.querySelector('.region-content');
 
-exploreRegionContentBtn.addEventListener("click", function () {
-  if (exploreRegionContent.classList.contains("region-content")) {
-    exploreRegionContent.classList.remove("region-content");
-    exploreRegionContent.classList.add("region-content-expand");
-    exploreRegionContentBtn.textContent = "Read Less";
-    exploreRegionContentButton.style.margin = "0px";
+readMoreBtn.addEventListener('click', () =>  {
+  if (regionContent.classList.contains('region-content-expand')) {
+    regionContent.classList.remove('region-content-expand')
+    readMoreBtn.innerHTML = "Read More"
   } else {
-    exploreRegionContent.classList.add("region-content");
-    exploreRegionContentBtn.textContent = "Read More";
-    exploreRegionContentButton.style.margin = "30px";
+    regionContent.classList.add('region-content-expand')
+    readMoreBtn.innerHTML = "Read Less"
   }
-});
+})
